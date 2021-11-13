30227deda31054da9eedc7637ce7e0bb34c35f1b
Jonathan Chen <jchen1199@gmail.com>
2021-11-13 18:45:49 -0500

    Update DB

--- tables/provinces.sql
+++ tables/provinces.sql
@@ -1,5 +1,6 @@
 CREATE TABLE provinces (
-	province_code VARCHAR  ( 50 ) PRIMARY KEY,
-	name VARCHAR ( 50 ) NOT NULL,
-  energy_profile_code INTEGER REFERENCES energy_profiles (energy_profile_code)
+	province_code SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	abbreviation varchar(10) NOT NULL,
+	energy_profile_code INTEGER REFERENCES energy_profiles(energy_profile_code)
 );
